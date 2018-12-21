b90f7eeb2b4e44ef24a3d78b8d60797a20b3fb31
ahmed-abdelazim <34234821+ahmed-abdelazim@users.noreply.github.com>
2018-12-21 16:15:37 +0200

    test shell scropt
    Video conversion

---
+++ test.sh
@@ -0,0 +1,13 @@
+ffmpeg \
+-loop 1 -t 5 -i 001.jpg \
+-loop 1 -t 5 -i 002.jpg \
+-loop 1 -t 5 -i 003.jpg \
+-loop 1 -t 5 -i 004.jpg \
+-loop 1 -t 5 -i 005.jpg \
+-filter_complex \
+"[0:v]fade=t=out:st=4:d=1[v0]; \
+ [1:v]fade=t=in:st=0:d=1,fade=t=out:st=4:d=1[v1]; \
+ [2:v]fade=t=in:st=0:d=1,fade=t=out:st=4:d=1[v2]; \
+ [3:v]fade=t=in:st=0:d=1,fade=t=out:st=4:d=1[v3]; \
+ [4:v]fade=t=in:st=0:d=1,fade=t=out:st=4:d=1[v4]; \
+ [v0][v1][v2][v3][v4]concat=n=5:v=1:a=0,format=yuv420p[v]" -map "[v]" out.mp4
